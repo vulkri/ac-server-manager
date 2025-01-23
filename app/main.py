a584787e0f9565af326735558f191fc721b8af8f
vulkrii <kamil.pogoda2+github@gmail.com>
2025-01-23 16:42:48 +0100

    WIP: uploads

--- app/main.py
+++ app/main.py
@@ -8,15 +8,20 @@ import threading
 import starlette.status as status
 from sqlalchemy.orm import Session
 
-from fastapi import Body, Depends, FastAPI, Request
+from fastapi import Body, Depends, FastAPI, HTTPException, Request, UploadFile
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse, PlainTextResponse, RedirectResponse
+from fastapi.responses import HTMLResponse, JSONResponse, PlainTextResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 
 from database import engine, SessionLocal
 import models
 from server_management import background_monitor, start_server
 
+
+UPLOAD_DIRECTORY = "./uploads"
+
+# Ensure the upload directory exists
+os.makedirs(UPLOAD_DIRECTORY, exist_ok=True)
 
 
 app = FastAPI()
@@ -75,7 +80,25 @@ def get_car_list(request: Request, db: Session=Depends(get_db)):
         request=request, name="serverlist.html", context=context
     )
 
-# Add car
+
+# Upload cars data 
+@app.put("/cars/upload/", response_class=JSONResponse)
+async def upload_cars(file: UploadFile, db: Session=Depends(get_db)):
+    file_path = os.path.join(UPLOAD_DIRECTORY, file.filename)
+    try:
+        with open(file_path, "wb") as f:
+            content = await file.read()  # Read the uploaded file content
+            f.write(content)  # Save the content to the specified path
+        return JSONResponse(content={"message": f"File '{file.filename}' uploaded successfully"}, status_code=201)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error saving file: {str(e)}")
+
+# Add 
+
+# Import cars
+@app.get("/cars/import/", response_class=HTMLResponse)
+def import_cars(request: Request):
+    
 
 # Delete car
 
